c0deb187ac46f705606d051019d78649e2ea357c
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-11-26 21:09:04 +0000

    Actually working code

--- SSH_Compare show run with local file.py
+++ SSH_Compare show run with local file.py
@@ -1,34 +1,90 @@
-# First proper stab at comparing the show run command with a local file but if the code here is correct then the output of the command should be saved to a file and compared with a local file once SSH'd into the router
+# Whoever said this is the easier of the options to pick...you're full of shit lol
+
+"""
+TODO
+interactive commands in sed commands functions
+"""
 
 import paramiko
-import diffios
+import getpass
+import sys
 
-# Paramiko code to SSH into router and get output of show run command and save output to file
-host = '192.168.1.66'
-user = 'cisco'
-secret = 'cisco'
-port = 22
+#start the ssh connection 
+ssh_connection=paramiko.SSHClient()
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Set policy to use when connecting to servers without a known host key
-ssh.connect(hostname=host, username=user, password=secret, port=port)
-ssh.exec_command('terminal length 0')
-stdin, stdout, stderr = ssh.exec_command('show run')
-list = stdout.readlines()
-output = [line.rstrip() for line in list] # Above two lines get rid of white space allowing more readable text file
-#print (''.join(output))
-file = open('sh_run.txt', 'w')
-file.write('\n'.join(output))
-file.close()
+def connect(ip_address, username, port=22):
+    global ssh_connection
+    try:
+        password = getpass.getpass("What is the password of the user of which you would like to connect to?")
+        #trusts all connections
+        ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print("*****Connecting...*****")
+        #gets the information neded to make the ssh connection
+        ssh_connection.connect(hostname = ip_address, username = username,password = password, port = port)
+        #if the connection information is wrong, catch the error and close the program
+        
+    except paramiko.AuthenticationException:
+        print("*****Invalid details provided, please check the details and try again*****")
+        sys.exit()
+        
+    except KeyboardInterrupt:
+        print("\nSSH session closed")
+        #exit program
+        sys.exit()
+            
+    print("*****Connected!*****")
+    return
 
-# diffios code meant to compare cisco ios files and output differences between them
-output = "sh_run.txt"
-local_file = "sh_run_local.txt"
+def writerunningconfig(text):
+    file = open('sh_run.txt', 'w')
+    for x in text:
+        file.write (x + '\n')
+    file.close()
 
-# We initialise a diffios Compare() object with our two files.
-diff = diffios.Compare(output, local_file)
-# From this Compare object we can see the differences between our
-# configurations using the delta() method.
-print(diff.delta())
---- output
-+++ local_file
+def sendcommand(command, printTF, cmdin=None):
+    global ssh_connection
+    #stdin is used for commands requiring inputs
+    #stdout gives the output of the command
+    #stderr shows any errors
+    stdin, stdout, stderr = ssh_connection.exec_command (command)
+    list = stdout.readlines()
+    output = [line.rstrip() for line in list] # Above two lines get rid of white space allowing more readable text file
+    for x in output:
+        print(x)
+    if printTF == True:
+        writerunningconfig (output)
+    #print(output)
+    #stdin.write("{}\n".format(cmdin))
+    return(output)
+
+def close():
+    ssh_connection.close()
+
+def localFile():
+    file = open('test.txt', 'r')
+    lines = file.readlines()
+    newconfig = []
+    for x in lines:
+        newconfig.append(x.rstrip())
+    #print (newconfig)
+    return newconfig
+
+def compare(routerConfig, grabbedOutput):
+    print ("starting Comparison")
+    line = 0
+    for x in routerConfig:
+        if routerConfig[line] != grabbedOutput[line]:
+            print("Diffence detected. Line " + str(line) + " in router config: " + routerConfig[line] + "\nfile configuration: "+ grabbedOutput[line])
+        line += 1
+
+#For testing purposes only
+if __name__ == '__main__':
+    connect("192.168.1.1", "admin")
+    routerConfig = sendcommand("sh run" , True)
+    close()
+    grabbedOutput = localFile()
+    compare(routerConfig, grabbedOutput)
+    #print (routerConfig)
+    #print ('\n')
+    #print('\n')
+    #print (grabbedOutput)
