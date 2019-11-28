c8a8050127d50df6ba51c76460b591dcf182ac9e
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-11-28 20:12:29 +0000

    Update SSH_Compare show run with local file.py

--- SSH_Compare show run with local file.py
+++ SSH_Compare show run with local file.py
@@ -1,90 +1,64 @@
-# Whoever said this is the easier of the options to pick...you're full of shit lol
-
-"""
-TODO
-interactive commands in sed commands functions
-"""
-
-import paramiko
 import getpass
-import sys
+from SSH_Connect import SSH
 
-#start the ssh connection 
-ssh_connection=paramiko.SSHClient()
 
-def connect(ip_address, username, port=22):
-    global ssh_connection
+def get_object(ip_address, username, port=22):
     try:
         password = getpass.getpass("What is the password of the user of which you would like to connect to?")
-        #trusts all connections
-        ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        print("*****Connecting...*****")
-        #gets the information neded to make the ssh connection
-        ssh_connection.connect(hostname = ip_address, username = username,password = password, port = port)
-        #if the connection information is wrong, catch the error and close the program
-        
-    except paramiko.AuthenticationException:
-        print("*****Invalid details provided, please check the details and try again*****")
-        sys.exit()
-        
+        return SSH(ip_address = ip_address, username = username, password = password, port = port)
+
     except KeyboardInterrupt:
         print("\nSSH session closed")
         #exit program
         sys.exit()
-            
-    print("*****Connected!*****")
-    return
+
 
 def writerunningconfig(text):
     file = open('sh_run.txt', 'w')
-    for x in text:
-        file.write (x + '\n')
+    file.write (text)
     file.close()
 
-def sendcommand(command, printTF, cmdin=None):
-    global ssh_connection
+
+def sendcommand(ssh, command, printTF, cmdin=None):
     #stdin is used for commands requiring inputs
     #stdout gives the output of the command
     #stderr shows any errors
-    stdin, stdout, stderr = ssh_connection.exec_command (command)
-    list = stdout.readlines()
-    output = [line.rstrip() for line in list] # Above two lines get rid of white space allowing more readable text file
-    for x in output:
-        print(x)
+    output, error = ssh.send_command(command, cmdin)
     if printTF == True:
-        writerunningconfig (output)
+        writerunningconfig (output.decode('UTF-8'))
     #print(output)
     #stdin.write("{}\n".format(cmdin))
-    return(output)
+    return(output, error)
 
-def close():
-    ssh_connection.close()
 
 def localFile():
     file = open('test.txt', 'r')
     lines = file.readlines()
     newconfig = []
     for x in lines:
-        newconfig.append(x.rstrip())
+        newconfig.append(x)
     #print (newconfig)
     return newconfig
 
+
 def compare(routerConfig, grabbedOutput):
     print ("starting Comparison")
-    line = 0
-    for x in routerConfig:
-        if routerConfig[line] != grabbedOutput[line]:
-            print("Diffence detected. Line " + str(line) + " in router config: " + routerConfig[line] + "\nfile configuration: "+ grabbedOutput[line])
-        line += 1
+    to_compare = routerConfig.splitlines(True)
+    i = 0
+    j = 0
+    while i< len(to_compare) and j<len(grabbedOutput):
+        if to_compare[i] != grabbedOutput[j]:
+            print("Diffence detected. Line " + str(i) + " in router config: " + to_compare[i] + "\nfile configuration: "+ grabbedOutput[j])
+        i += 1
+        j += 1
+    if i != j:
+        print("Files unequal")
 
-#For testing purposes only
+
 if __name__ == '__main__':
-    connect("192.168.1.1", "admin")
-    routerConfig = sendcommand("sh run" , True)
-    close()
+    ssh = get_object('192.168.1.1', 'admin')
+    ssh.connect()
+    output, errors = sendcommand(ssh, "sh run", True)
     grabbedOutput = localFile()
-    compare(routerConfig, grabbedOutput)
-    #print (routerConfig)
-    #print ('\n')
-    #print('\n')
-    #print (grabbedOutput)
+    compare(output.decode('UTF-8'), grabbedOutput)
+    ssh.close()
