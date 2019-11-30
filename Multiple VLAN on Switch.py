799b8b4b59703481b21aa0d32a5cd8cd37cf6d8e
Yellow-Tail <lewishughes1234@gmail.com>
2019-11-30 22:54:11 +0000

    Function to configure vlans on a switch

--- Multiple VLAN on Switch.py
+++ Multiple VLAN on Switch.py
@@ -1,30 +1,17 @@
-#uses file 'deivces' which can be found on this commit too
 from SSH_Connect import SSH
 
-switches_list = []
+def configure_switch_vlan(ip_address, username, password):
+    vlan_config = SSH(ip_address,username,password)
+    vlan_config.connect()
 
-file = open("devices", "r")
-for line in file:
-    switch_info_list = line.strip().split(',')
-    #test what is being read
-#    print(switch_info_list[0], switch_info_list[1], switch_info_list[2])
-    test = SSH(switch_info_list[0], switch_info_list[1], switch_info_list[2])
-    test.connect()
+    vlan_config.send_command("en")
+    vlan_config.send_command("conf t")
+    vlan_config.send_command("vlan 10")
+    vlan_config.send_command("name IT")
+    vlan_config.send_command("vlan 20")
+    vlan_config.send_command("name Sales")
+    vlan_config.send_command("vlan 30")
+    vlan_config.send_command("name Marketing")
+    vlan_config.send_command("end")
 
-    #passwords to be run on each device connected to
-    output, errors = test.send_command("en", "PASSWORD")
-    output, errors = test.send_command("vlan 10")
-    output, errors = test.send_command("name Finance")
-    output, errors = test.send_command("vlan 20")
-    output, errors = test.send_command("name IT")
-    output, errors = test.send_command("vlan 30")
-    output, errors = test.send_command("name Sales")
-    output, errors = test.send_command("exit")
-
-    #print any errors
-    print(output,errors)
-    #close ssh session
-    test.close()    
-        
-#close the file
-file.close()
+configure_switch_vlan("192.168.0.1", "admin", "cisco")
