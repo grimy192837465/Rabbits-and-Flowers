efce37e15d8bfcab38d241575790295750933905
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-12-04 14:31:56 +0000

    Update eigrp_test.py

--- eigrp_test.py
+++ eigrp_test.py
@@ -12,10 +12,9 @@ def eigrp_configuration(ip_address, username, password):
 
     #Calls the function within the SSH_Connect file called send_configuration which is meant for opening config files
     #Below line will open the config file for eigrp called EIGRP_Configs as set to True as by default in the SSH_Connect file it is set to false
-    eigrp_conf.send_configuration(from_file=True, configuration="EIGRP_Configs")
+    eigrp_conf.send_configuration(from_file=True, configuration="EIGRP_Configs.txt")
 
     #IP address, username and password for the router that is needed for the SSH conneciton to be successful
-    eigrp_configuration("192.168.1.1", "admin", "adminpass")
-    eigrp_configuration("192.168.1.2", "admin", "adminpass")
+eigrp_configuration("192.168.1.1", "admin", "adminpass")
+eigrp_configuration("192.168.1.2", "admin", "adminpass")
 
-    
