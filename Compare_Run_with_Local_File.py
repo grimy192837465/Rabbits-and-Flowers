4bea4c6431c4a6b577c2a453b424fe2455f823d2
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-12-07 21:08:52 +0000

    Formatted to PEP 8 requirements
    Formatted the code to follow the PEP 8 requirements, will need to add more comments to make more readable in the future.

--- Compare_Run_with_Local_File.py
+++ Compare_Run_with_Local_File.py
@@ -4,51 +4,62 @@ from SSH_Connect import SSH
 
 def get_object(ip_address, username, port=22):
     try:
-        password = getpass.getpass("What is the password of the user of which you would like to connect to?")
-        return SSH(ip_address = ip_address, username = username, password = password, port = port)
+        password = getpass.getpass(
+            "What is the password of the user of which you would like to connect to?"
+        )
+        return SSH(
+            ip_address=ip_address, username=username, password=password, port=port
+        )
 
     except KeyboardInterrupt:
         print("\nSSH session closed")
-        #exit program
+        # exit program
         sys.exit()
 
 
 def writerunningconfig(text):
-    file = open('sh_run.txt', 'w')
-    file.write (text)
+    file = open("sh_run.txt", "w")
+    file.write(text)
     file.close()
 
 
 def sendcommand(ssh, command, printTF):
-    #stdin is used for commands requiring inputs
-    #stdout gives the output of the command
-    #stderr shows any errors
+    # stdin is used for commands requiring inputs
+    # stdout gives the output of the command
+    # stderr shows any errors
     output = ssh.send_command(command)
     if printTF == True:
-        writerunningconfig (output)
-    #print(output)
-    #stdin.write("{}\n".format)
+        writerunningconfig(output)
+    # print(output)
+    # stdin.write("{}\n".format)
     return output
 
 
 def localFile():
-    file = open('test.txt', 'r')
+    file = open("test.txt", "r")
     lines = file.readlines()
     newconfig = []
     for x in lines:
         newconfig.append(x)
-    #print (newconfig)
+    # print (newconfig)
     return newconfig
 
 
 def compare(routerConfig, grabbedOutput):
-    print ("starting Comparison")
+    print("starting Comparison")
     to_compare = routerConfig.splitlines(True)
     i = 0
     j = 0
-    while i< len(to_compare) and j<len(grabbedOutput):
+    while i < len(to_compare) and j < len(grabbedOutput):
         if to_compare[i] != grabbedOutput[j]:
-            print("Difference detected. Line " + str(i) + " in router config: " + to_compare[i] + "\nfile configuration: "+ grabbedOutput[j])
+            print(
+                "Difference detected. Line "
+                + str(i)
+                + " in router config: "
+                + to_compare[i]
+                + "\nfile configuration: "
+                + grabbedOutput[j]
+            )
         i += 1
         j += 1
     if i != j:
@@ -64,10 +75,10 @@ def compare_run_with_local_file(host, username, password, secret):
     ssh.close()
 
 
-if __name__ == '__main__':
-    ssh = get_object('192.168.1.1', 'admin')
+if __name__ == "__main__":
+    ssh = get_object("192.168.1.1", "admin")
     ssh.connect()
     output = sendcommand(ssh, "sh run", True)
     grabbedOutput = localFile()
-    compare (output, grabbedOutput)
+    compare(output, grabbedOutput)
     ssh.close()
