4d9ab2985a042d9b6df31facc5969ac0f50eefa7
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-12-08 10:13:38 +0000

    Update config_rip.py

--- config_rip.py
+++ config_rip.py
@@ -1,17 +1,19 @@
 from SSH_Connect3 import SSH
 
-#Function is called to configure the routers
+# Function is called to configure the routers
 def configure_rip(ip_address, username, password):
-    #creates the ssh connection which then saves it to a variable
-    rip_config = SSH(ip_address,username,password)
-    
-    #connects to the router
-    rip_config.connect()
-    
-    #configurations from the rip_routers_config file
-    rip_config.send_configuration(from_file=True, configuration="rip_routers_config.txt")
+    # creates the ssh connection which then saves it to a variable
+    rip_config = SSH(ip_address, username, password)
 
-#test the rip function
+    # connects to the router
+    rip_config.connect()
+
+    # configurations from the rip_routers_config file
+    rip_config.send_configuration(
+        from_file=True, configuration="rip_routers_config.txt"
+    )
+
+
+# test the rip function
 configure_rip("192.168.0.1", "admin", "cisco")
 configure_rip("192.168.0.2", "admin", "cisco")
-
