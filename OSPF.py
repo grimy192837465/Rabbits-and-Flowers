214fa7dfd8f5a0f90f406090eb5c8718756e1ddb
grimy192837465 <42099648+grimy192837465@users.noreply.github.com>
2019-12-11 17:07:24 +0000

    Update OSPF.py

--- OSPF.py
+++ OSPF.py
@@ -1,15 +1,16 @@
 from SSH_Connect import SSH
 
 # the function named for OSPF Configuration
-def configure_ospf(ip_address, username, password):
+def configure_ospf(ip_address, username, password, secret=""):
     # Starts SSH Connection for device
-    configure_ospf = SSH(ip_address, username, password)
+    configure_ospf = SSH(ip_address, username, password, secret=secret)
     # Should create secure connection
     configure_ospf.connect()
 
-    configure_ospf.send_configuaration(from_file=True, configuration="OSPF_Config")
+    configure_ospf.send_configuration(from_file=True, configuration="OSPF_Config")
 
 
 # example function for test
-configure_ospf("192.168.0.1", "admin", "cisco")
-configure_ospf("192.168.0.2", "admin", "cisco")
+if __name__ == "__main__":
+    configure_ospf("192.168.0.1", "admin", "cisco")
+    configure_ospf("192.168.0.2", "admin", "cisco")
